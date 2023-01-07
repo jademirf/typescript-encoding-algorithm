7ebd49288674e665f3b6dbb9dee0f4252a04664c
Jademir de Moura <jademirf@gmail.com>
2023-01-07 12:59:38 -0300

    not finished...

--- src/utils/normalizer.spec.ts
+++ src/utils/normalizer.spec.ts
@@ -1,13 +1,14 @@
 import { normalize } from './normalizer';
 describe('Normalize the string', () => {
-  it('should return text with 64 char at maximum', () => {
-    const originalText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae pretium ante. Morbi ut risus varius, varius mauris ac, ullamcorper libero. Sed laoreet laoreet nisi a accumsan. Donec a ex eu elit tempor auctor pellentesque eget sapien. Aenean auctor, urna eget congue posuere, tellus sem convallis diam, id semper arcu erat nec mauris. Curabitur sit amet nibh ut elit congue vehicula. Praesent elit urna, imperdiet a enim a, varius congue mauris. Mauris accumsan sapien non nisl lacinia maximus. Mauris augue elit, accumsan quis turpis at, blandit faucibus ex. Sed ut odio et libero ornare aliquet et vel turpis."
 
-    const normalizedText = normalize(originalText)
+  it('should return the text broken into rows', () => {
+    const colLength = 4
+    const originalText = "abcdefghijklmn"
 
-    expect(normalizedText).toHaveLength(64)
+    const normalizedArray = normalize(originalText)
+    expect(normalizedArray).toHaveLength(4)
+    expect(normalizedArray[0]).toHaveLength(colLength)
+    expect(normalizedArray[0].length).toBeLessThanOrEqual(normalizedArray.length)
   })
-
-  // it('should return a text as a rectangle')
 
 })
--- src/utils/normalizer.ts
+++ src/utils/normalizer.ts
@@ -1,9 +1,41 @@
  export function normalize (rawText:string): string {
   let normalText = ''
+  const textArray = []
+  let cols = 0
 
   if(rawText.length > 64) {
     normalText = rawText.substring(0, 64)
+  } else {
+    normalText = rawText
   }
 
-  return normalText
+  for(let i = 1; i <= 8; i++) {
+    if (normalText.length <= (i*i)) {
+      cols = i
+      break
+    }
+  }
+  const fullColumns = (normalText.length/cols)
+  
+  for(let i = 0; i <= fullColumns; i++){
+    let chunk = ''
+    chunk = rawText.substring(i*cols,(i+1)*cols)
+    textArray.push(chunk)
+  }
+
+  console.log(textArray)
+
+  let result = ''
+  for (let i = 0; i <= cols; i++) {
+    for (let j = 0; j <= textArray.length; j++) {
+      if(textArray[j]) {
+        result += textArray[j].substring(i)
+      }
+      if (result.length == cols) {
+        result += ' '
+      }
+    }
+  }
+
+  return result
 }
